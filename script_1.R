779b386f5afd8a96d866d21f40ad15df016f30eb
Luke Pascual <lpascual7980@gmail.com>
2017-06-12 11:56:19 -0400

    Rastered GPW & 2013 DMSP data into buffer zones.

--- script_1.R
+++ script_1.R
@@ -54,15 +54,27 @@ proj4string(rdbuff)
 rdbuff <- spTransform(rdbuff, CRS("+proj=utm +zone=36 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 plot(rdbuff)
 rdbuff_f <- fortify(rdbuff)
+#rdbuff_f1 <- subset(rdbuff_f, id == 0)
 
 rdntwrk <- ggplot() + geom_map(data = governorates_f, aes(x=long, y=lat, map_id = id), map = governorates_f, alpha = .5, col = "black")
-rdntwrk <- rdntwrk + geom_map(data = roads_f, aes(x=long, y=lat, map_id = id), map = roads_f, alpha = 0, col= "blue")
+#rdntwrk <- rdntwrk + geom_map(data = roads_f, aes(x=long, y=lat, map_id = id), map = roads_f, alpha = 0, col= "blue")
 rdntwrk <- rdntwrk + geom_map(data = roads_proj_f, aes(x=long, y=lat, map_id = id), map = roads_proj_f, alpha = 0, col= "red")
 rdntwrk <- rdntwrk + geom_map(data = rdbuff_f, aes(x=long, y=lat, map_id = id), map = rdbuff_f, alpha = 0, col= "yellow")
+rdntwrk <- rdntwrk + geom_point(size = .5, shape = 18, aes(x = 716245.1, y = 3514522))
+rdntwrk <- rdntwrk + geom_point(size = .5, shape = 17, aes(x = 716271.4, y = 3514513))
 rdntwrk
 
 
-unionSpatialPolygons()
 
+popdata <- raster("~/Google Drive/LiberiaProject/gpw-v4-population-count_2015.tif")
+buffpop <- extract(popdata, rdbuff, df = TRUE)
+buffpopdf <- data.frame(buffpop)
+buffpop
 
+buffpop_sum <- aggregate(. ~ ID, buffpop, sum)
+buffpopLT13_sum <- aggregate(. ~ ID, buffpopLT, sum)
+buffpopLT13 <- buffpopLT ## .RData file copied from DMSP data ##
 
+#remove("rdbuff_f1")
+
+#unionSpatialPolygons(rdbuff, id)
